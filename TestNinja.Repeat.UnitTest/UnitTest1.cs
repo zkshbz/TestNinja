e03f701615051281c4ec063fbbc7b97d8780f464
zkshbz <zekisahbaz@outlook.com>
2023-03-22 09:51:02 +0300

    TestCommit

---
+++ TestNinja.Repeat.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestNinja.Repeat.UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
